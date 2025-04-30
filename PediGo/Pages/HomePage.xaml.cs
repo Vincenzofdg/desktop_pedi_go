180d0a3f983d12560357e093c362bb39c81efc28
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-04-30 11:49:20 -0300

    feat: 3 seconds update

--- PediGo/Pages/HomePage.xaml.cs
+++ PediGo/Pages/HomePage.xaml.cs
@@ -5,29 +5,50 @@ namespace PediGo.Pages;
 
 public partial class HomePage : ContentPage
 {
+    private CancellationTokenSource _cts = new();
+
     public HomePage()
     {
         InitializeComponent();
+
+        // Inicia a rotina de atualização
+        StartAutoUpdateLoop();
+    }
+
+    private async void StartAutoUpdateLoop()
+    {
+        while (!_cts.IsCancellationRequested)
+        {
+            AtualizarBotoes();
+            await Task.Delay(3000); // Aguarda 3 segundos
+        }
+    }
+
+    private void AtualizarBotoes()
+    {
+        //Debug.WriteLine("CHAMANDO ATUALIZACAO");
+        CommandContainer.Children.Clear();
+
         int[] commandNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-        foreach (var num in commandNumbers) {
+        foreach (var num in commandNumbers)
+        {
             var btn = new CommandButton(num.ToString(), OnCounterClicked);
- 
             CommandContainer.Children.Add(btn);
         }
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        var clickedButton = sender as CommandButton;
-        if (clickedButton != null)
+        if (sender is CommandButton clickedButton)
         {
             DisplayAlert("Debug", $"Você clicou no botão: {clickedButton.Name}", "OK");
         }
-        else
-        {
-            DisplayAlert("Erro", "O sender não é um CommandButton", "OK");
-        }
     }
 
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _cts.Cancel(); // Para a tarefa quando a página for fechada
+    }
 }
