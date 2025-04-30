9404f7d23c01f2deaa3c5246007eb3b8f4826ecb
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-04-30 16:07:14 -0300

    feat: orders action

---
+++ PediGo/Data/Actions/OrderAction.cs
@@ -0,0 +1,10 @@
+﻿namespace PediGo.Data.Actions
+{
+    public class OrderAction : SqliteServer
+    {
+        public async Task<Orders[]> GetOrderItems()
+        {
+            return await _connection.Table<Orders>().ToArrayAsync();
+        }
+    }
+}
