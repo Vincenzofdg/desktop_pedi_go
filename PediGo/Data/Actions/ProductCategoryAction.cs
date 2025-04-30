20bfa8eb5e3df46d4366ce04b0665bf132def32f
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-04-30 16:08:51 -0300

    feat: product category action

---
+++ PediGo/Data/Actions/ProductCategoryAction.cs
@@ -0,0 +1,11 @@
+﻿namespace PediGo.Data.Actions
+
+{
+    public class ProductCategoryAction : SqliteServer
+    {
+        public async Task<ProductCategories[]> GetOrderItems()
+        {
+            return await _connection.Table<ProductCategories>().ToArrayAsync();
+        }
+    }
+}
