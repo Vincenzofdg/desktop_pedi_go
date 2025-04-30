b69d6b79687955f896d9ab6fc51fa8451b3ed8e4
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-04-30 16:28:47 -0300

    feat: product categories by categoryId

--- PediGo/Data/Actions/ProductCategoryAction.cs
+++ PediGo/Data/Actions/ProductCategoryAction.cs
@@ -3,9 +3,17 @@
 {
     public class ProductCategoryAction : SqliteServer
     {
-        public async Task<ProductCategories[]> GetOrderItems()
+        public async Task<ProductCategories[]> GetProductCategories()
         {
             return await _connection.Table<ProductCategories>().ToArrayAsync();
+        }
+
+        public async Task<ProductCategories[]> GetOrderItemsById(long categoryId)
+        {
+            var query = @"SELECT * FROM ProductCategories WHERE CategoryId = ?";
+            var result = await _connection.QueryAsync<ProductCategories>(query, categoryId);
+            
+            return [..result];
         }
     }
 }
